a441e371dd1aa9ccdf806088f78f04ec7cd89e14
Jesse S <3428918+jjs2484@users.noreply.github.com>
2020-08-10 22:34:36 -0400

    fix broken file path

--- includes/class-emr.php
+++ includes/class-emr.php
@@ -237,7 +237,7 @@ class EMR {
 		// Cookie empty then include.
 		if ( empty( $full_site_cookie ) ) {
 			if ( ! class_exists( 'Mobile_Detect' ) ) {
-				require_once __DIR__ . '/Mobile_Detect.php';
+				require_once( dirname(__FILE__) . '/Mobile_Detect.php' );
 			}
 			$detect = new Mobile_Detect();
 			// EMR option page settings.
